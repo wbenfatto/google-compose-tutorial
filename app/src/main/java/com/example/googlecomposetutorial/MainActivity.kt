8f9b107a63730ad098cb370554d010babe19f4ed
William Benfatto <william@mobile.dev.br>
2022-12-17 12:02:30 -0300

    Layouts

--- app/src/main/java/com/example/googlecomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/googlecomposetutorial/MainActivity.kt
@@ -3,26 +3,50 @@ package com.example.googlecomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(name = "William")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String){
-    Text(text = "Hello $name")
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+
 }
 
 @Preview
 @Composable
-fun PreviewMessageCard(){
-    MessageCard("Android")
+fun PreviewMessageCard() {
+    MessageCard(msg = Message("Colleague", "Hey, Take a look at Jetpack Compose, it's great!"))
 }
